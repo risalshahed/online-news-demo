d1f43b81c6baacfb2ea5eb6af428649a5ef22af7
SShipon <nxshipon@gmail.com>
2024-03-15 12:52:24 +0600

    fackbook link and youtub updted

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black  rounded-box w-52">
        {navItems}
       </ul>
     </div>
@@ -39,10 +39,10 @@ export default function Navbar() {
      {navItems}
     </ul>
   </div>
-  <div className="navbar-end gap-1">
-    <a className="btn">facebook</a>
-    <a className="btn">Youtube</a>
-    <a className="btn">Google</a>
+  <div className="navbar-end gap-4">
+    <a className="text-2xl"> <i class="fa-brands fa-facebook"></i></a>
+    <a className="text-2xl"><i class="fa-brands fa-youtube"></i></a>
+    <a className="text-2xl"> <i class="fa-brands fa-instagram"></i></a>
   </div>
 </div>
      </>
