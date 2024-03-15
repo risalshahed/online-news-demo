dff977185d70ff4237ad3920071568de3ae7e7c5
SShipon <nxshipon@gmail.com>
2024-03-15 11:48:39 +0600

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 import { Route, Routes } from 'react-router-dom'
-import Home from './componentes/Home/Home';
+import Home from './components/Home/Home'
+import Navbar from './Pages/Navbar/Navbar'
+
 
 export default function App() {
   return (
-    <>
+   <>
       <div className="app">
+      <Navbar />
         <Routes>
           <Route path="/" Component={Home} />
         </Routes>
       </div>
-    </>
+      </>
   )
 }
